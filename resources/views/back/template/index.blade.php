f5c42919b6fba35d14ab06486293e0196d0a7c57
Bagas K <bagaskm36@gmail.com>
2025-09-07 15:09:48 +0700

    logout route on topbar fixed

--- resources/views/back/template/index.blade.php
+++ resources/views/back/template/index.blade.php
@@ -114,7 +114,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="logoutModalLabel">Meninggalkan Check Your Heart</h5>
+            <h5 class="modal-title" id="logoutModalLabel">Meninggalkan Aplikasi Kalibrasi Digital</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -122,7 +122,7 @@
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
-            <form method="POST" action="#">
+            <form method="POST" action="{{ route('logout') }}">
             @csrf
             <button type="submit" class="btn btn-danger">Logout</button>
             </form>
